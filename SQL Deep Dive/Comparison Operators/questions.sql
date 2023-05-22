73d13d394b01a37e98e2cb3a457e9208b0744b58
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-05-21 21:50:28 -0400

    Update questions.sql

--- SQL Deep Dive/Comparison Operators/questions.sql
+++ SQL Deep Dive/Comparison Operators/questions.sql
@@ -4,9 +4,9 @@ SELECT COUNT(*) FROM customers
     gender = 'F';
 
 -- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
-/*
-* Write your query here
-*/
+SELECT age,income FROM customers
+    WHERE age > 44 AND
+    income >= 100000;
 
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
