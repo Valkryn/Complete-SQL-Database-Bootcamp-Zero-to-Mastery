6d8b95e7075869f0cdc17d5618cb8440196acb33
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-05-25 12:09:29 -0400

    Update questions.sql

--- SQL Deep Dive/Null Values/questions.sql
+++ SQL Deep Dive/Null Values/questions.sql
@@ -5,6 +5,9 @@
 * Assuming a students minimum age for the class is 15, what is the average age of a student?
 */
 
+SELECT AVG(COALESCE(AGE,15)) from "Student";
+
+
 /*
 * DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
 * Question: 
