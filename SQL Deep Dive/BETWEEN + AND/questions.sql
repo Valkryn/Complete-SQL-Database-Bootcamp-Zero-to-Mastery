c08c8a915978f6cb5421966a50271fb240f3f33e
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-05-25 21:05:03 -0400

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -1,9 +1,9 @@
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
 
-/*
-* Write your query here
-*/
+SELECT age, firstname, lastname, income FROM CUSTOMERS
+    WHERE age between 30 and 50
+        and income < 50000;
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
