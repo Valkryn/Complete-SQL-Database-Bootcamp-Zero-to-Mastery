4e927cb84e3e75bdb6d393f4133e138f8070cef4
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-09-27 18:06:20 -0400

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,6 +3,8 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
+select * from public.employees
+    order by first_name , last_name DESC;
 
 
 /*
@@ -10,10 +12,14 @@
 * Table: employees
 * Question: Sort employees by age
 */
-
+SELECT EXTRACT(YEAR FROM AGE(birth_date)) AS "age" FROM public.employees
+    ORDER BY birth_date desc;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+select first_name , hire_date from public.employees
+    where first_name like 'K%'
+    ORDER BY hire_date;
