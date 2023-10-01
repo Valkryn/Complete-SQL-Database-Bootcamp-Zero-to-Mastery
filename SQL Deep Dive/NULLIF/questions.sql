104722d011928554a6a7c44b776c20d0d39a4d70
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-10-01 19:20:53 -0400

    Update questions.sql

--- SQL Deep Dive/NULLIF/questions.sql
+++ SQL Deep Dive/NULLIF/questions.sql
@@ -4,4 +4,4 @@
 * Question: Show NULL when the product is not on special (0)
 */
 
-select * ,nullif (special , 0) as "it is what it is`" from products;
+select * ,nullif (special , 0) as "Special`" from products;
